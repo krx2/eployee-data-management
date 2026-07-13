9ebcfc15e2ba83d6361ea56ddd113c69404d32f9
Konrad Zebro <konrad.zebro@gmail.com>
2026-07-13 22:23:12 +0200

    dependencies

--- src/main/java/com/krx2/employeedatamanagment/EmployeeDataManagmentApplication.java
+++ src/main/java/com/krx2/employeedatamanagment/EmployeeDataManagmentApplication.java
@@ -1,13 +1,13 @@
-package com.krx2.emplyeedatamanagment;
+package com.krx2.employeedatamanagment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmplyeeDataManagmentApplication {
+public class EmployeeDataManagmentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmplyeeDataManagmentApplication.class, args);
+        SpringApplication.run(EmployeeDataManagmentApplication.class, args);
     }
 
 }
